ba513dce55182b040658aa56d82340ca293dc83a
Jean0405 <Jean0405@outlook.es>
2022-12-30 15:45:11 -0500

    Login backend finalizado

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "proyecto";
+
+$conn =  mysqli_connect($dbhost, $dbuser,$dbpass, $dbname);
+
+if(!$conn){
+    die("No hay una conexión: ".mysqli_connect_error());
+}
+
+
+$usuario = $_POST["txtusuario"];
+$pass = $_POST["txtpassword"];
+
+$query = mysqli_query($conn, "SELECT * FROM login WHERE user ='".$usuario."' and password = '".$pass."'");
+$nr = mysqli_num_rows($query);
+
+if ($nr == 1) {
+    // header("Location: index.html");
+    echo "Bienvenido ".$usuario;
+    
+}else if($nr == 0){
+    echo "No hay ingreso";
+}
+
+?>
